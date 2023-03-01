578ddc74651451a5b794f43b00e4ce90f4212cf4
serhattomak <serhattomak@gmail.com>
2023-03-01 10:58:27 +0300

    Begining to classes Introduction to C# and the console programming according to class.

--- consoleprogramming/Program.cs
+++ consoleprogramming/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, World!");
+Console.WriteLine("Enter your name:");
+string name = Console.ReadLine();
+Console.WriteLine("Enter your surname:");
+string surname = Console.ReadLine();
+
+Console.WriteLine("Hello" +" "+ name + " " + surname);
