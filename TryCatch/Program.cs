99db8a1e7ce32b56bb009aed004c3fcdf7172d76
serhattomak <serhattomak@gmail.com>
2023-03-01 15:51:00 +0300

    Try Catch Learning the basics of the try catch.

---
+++ TryCatch/Program.cs
@@ -0,0 +1,42 @@
+﻿try
+{
+    Console.WriteLine("Enter a number:");
+    int sayi = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Number:" + sayi);
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Hata:" + ex.Message.ToString());
+    throw;
+}
+//finally
+//{
+//    Console.WriteLine("Process completed.");
+//}
+
+try
+{
+    //int a = int.Parse(null);
+    //int a=int.Parse("test");
+    int a = int.Parse("-20000000000");
+}
+catch (ArgumentNullException ex)
+{
+    Console.WriteLine("The value is empty.");
+    Console.WriteLine(ex);
+}
+catch(FormatException ex)
+{
+    Console.WriteLine("Value type is not valid");
+    Console.WriteLine(ex);
+}
+catch(OverflowException ex)
+{
+    Console.WriteLine("You've entered too small or too large value.");
+    Console.WriteLine(ex);
+}
+finally
+{
+    Console.WriteLine("Process completed successfully.");
+}
+
