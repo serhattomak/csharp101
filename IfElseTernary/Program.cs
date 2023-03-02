5f707e77d2d5d958fc1551d7d7e0a081168eadd6
serhattomak <serhattomak@gmail.com>
2023-03-02 18:06:49 +0300

    IfElseTernary Using the if, else if and ternary in basic examples.

---
+++ IfElseTernary/Program.cs
@@ -0,0 +1,13 @@
+﻿int time = DateTime.Now.Hour;
+
+if (time >= 6 && time < 11)
+    Console.WriteLine("Good Morning!");
+else if (time <= 18)
+    Console.WriteLine("Have a nice day!");
+else
+    Console.WriteLine("Good Night!");
+
+string result = time <= 18 ? "Have a nice day!" : "Good Night!";
+
+result = time >= 6 && time < 11 ? "Good Morning!" : time <=18 ? "Have a good day!" : "Good Night!";
+Console.WriteLine(result);
