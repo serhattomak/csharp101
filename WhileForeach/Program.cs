563120ac8a939355163c67ee922071bf1f3c56be
serhattomak <serhattomak@gmail.com>
2023-03-03 11:51:49 +0300

    While ForEach Learning the basics of the while and foreach.

---
+++ WhileForeach/Program.cs
@@ -0,0 +1,33 @@
+﻿//While
+//A programme that calculating the average of the numbers of 1 to value which is getting by the console. (Including the entered number)
+
+Console.WriteLine("Please enter a number: ");
+int number = int.Parse(Console.ReadLine());
+int counter = 1;
+int result = 0;
+
+while (counter <= number)
+{
+    result += counter;
+    counter++;
+}
+
+Console.WriteLine(result / number);
+
+//Write all the letter from 'a' to 'z' to the console.
+
+char character = 'a';
+while (character<='z')
+{
+    Console.WriteLine(character);
+    character++;
+}
+
+Console.WriteLine("***** Foreach *****");
+
+string[] cars = { "BMW", "Ford", "Toyota", "Nissan" };
+
+foreach (var car in cars)
+{
+    Console.WriteLine(car);
+}
