4610033fe9337c6d4c7738e9cfc8aed7dede8aa2
serhattomak <serhattomak@gmail.com>
2023-03-03 21:12:23 +0300

    Added do while loop.

--- WhileForeach/Program.cs
+++ WhileForeach/Program.cs
@@ -1,6 +1,6 @@
 ﻿//While
 //A programme that calculating the average of the numbers of 1 to value which is getting by the console. (Including the entered number)
-
+Console.WriteLine("***** While *****");
 Console.WriteLine("Please enter a number: ");
 int number = int.Parse(Console.ReadLine());
 int counter = 1;
@@ -17,11 +17,21 @@ Console.WriteLine(result / number);
 //Write all the letter from 'a' to 'z' to the console.
 
 char character = 'a';
-while (character<='z')
+while (character <= 'z')
 {
     Console.WriteLine(character);
     character++;
 }
+
+//Do While
+Console.WriteLine("***** Do While *****");
+int number1 = 1;
+
+do
+{
+    Console.WriteLine(number1);
+    number1++;
+} while (number1<10);
 
 Console.WriteLine("***** Foreach *****");
 
