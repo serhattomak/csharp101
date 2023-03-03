af84e90d642eaa437cff39b816cbb44009a1d3b6
serhattomak <serhattomak@gmail.com>
2023-03-03 22:51:20 +0300

    Arrays Using the arrays with for and foreach loops.

---
+++ Arrays/Program.cs
@@ -0,0 +1,34 @@
+﻿string[] colors = new string[5];
+
+string[] animals = { "Cat", "Dog", "Bird", "Monkey" };
+
+int[] array;
+array=new int[5];
+
+//Value assignments to the arrays
+
+colors[0] = "Blue";
+array[3] = 10;
+
+Console.WriteLine(array[3]);
+Console.WriteLine(animals[1]);
+Console.WriteLine(colors[0]);
+
+//Using loops with arrays
+//Calculating the average of the values entered with the keyboard
+Console.WriteLine("Please enter the dimension of the array: ");
+int dimension = int.Parse(Console.ReadLine());
+int[] numberArray = new int[dimension];
+
+for (int i = 0;i < dimension; i++)
+{
+    Console.Write("Please enter the next number ({0}): ", i+1);
+    numberArray[i] = int.Parse(Console.ReadLine());
+}
+
+int result = 0;
+foreach (var number in numberArray)
+{
+    result += number;
+}
+Console.WriteLine("Average: " + result / dimension);
