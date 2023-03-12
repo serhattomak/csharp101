b97b01602b498a498f6c6d347866d19e19e5799e
serhattomak <serhattomak@gmail.com>
2023-03-12 20:45:56 +0300

    Struct Learning the differance between struct and class, using them on a simple case.

---
+++ Struct/Program.cs
@@ -0,0 +1,42 @@
+﻿Rectangle rectangle = new Rectangle();
+rectangle.ShortEdge = 3;
+rectangle.LongEdge = 4;
+
+Console.WriteLine("Class Area Result: {0}", rectangle.CalculateArea());
+
+Rectangle_Struct rectangle_struct=new Rectangle_Struct(3,4);
+
+Console.WriteLine("Struct Area Result: {0}", rectangle_struct.CalculateArea());
+class Rectangle
+{
+    public int ShortEdge;
+    public int LongEdge;
+
+    public Rectangle()
+    {
+        ShortEdge = 3;
+        LongEdge = 4;
+    }
+
+    public long CalculateArea()
+    {
+        return this.ShortEdge * this.LongEdge;
+    }
+}
+
+struct Rectangle_Struct
+{
+    public int ShortEdge;
+    public int LongEdge;
+
+    public Rectangle_Struct(int shortEdge, int longEdge)
+    {
+        ShortEdge=shortEdge;
+        LongEdge=longEdge;
+    }
+
+    public long CalculateArea()
+    {
+        return this.ShortEdge * this.LongEdge;
+    }
+}
