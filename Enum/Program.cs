82c913714429a8f85a15bd875dc5f2ea9c9ea1e2
serhattomak <serhattomak@gmail.com>
2023-03-13 19:34:42 +0300

    Enum Learning how to use enum in simple cases.

---
+++ Enum/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine(Days.Sunday);
+Console.WriteLine((int)Days.Saturday);
+
+int temperature = 32;
+
+if (temperature<=(int)Weather.Normal)
+{
+    Console.WriteLine("We can wait a bit to go outside.");
+}
+else if (temperature>=(int)Weather.Hot)
+{
+    Console.WriteLine("It is very hot to go outside. ");
+}
+else if (temperature>=(int)Weather.Normal&&temperature<(int)Weather.VeryHot)
+{
+    Console.WriteLine("Let's go outside!");
+}
+
+enum Days
+{
+    Monday = 1,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday = 23,
+    Saturday,
+    Sunday
+}
+
+enum Weather
+{
+    Cold=5,
+    Normal=20,
+    Hot=25,
+    VeryHot=30
+}
